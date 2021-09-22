324e71e33cff2c9f491234a3c1fb4fcfb45dd8ad
Roman-Skabin <skabin.roman@mail.ru>
2021-09-22 22:45:17 +0300

    Updated license, added premake license

--- rev/core/work_queue.cpp
+++ rev/core/work_queue.cpp
@@ -1,6 +1,8 @@
-//
-// Copyright 2020-2021 Roman Skabin
-//
+// Copyright (c) 2020-2021, Roman-Skabin
+// All rights reserved.
+// 
+// This source code is licensed under the BSD-style license found in the
+// LICENSE.md file in the root directory of this source tree. 
 
 #include "core/pch.h"
 #include "core/work_queue.h"
